8450a5bcd706ad89a85963acd1aad60cdeeb51e8
Johana28297 <fernandezjoha21@gmail.com>
2025-09-17 15:22:52 -0600

    Clase Cliente, a la hora de registrar un cliente pedira Nombre, DUI y Telefono

--- HotelSunsetParadise/Models/Cliente.cs
+++ HotelSunsetParadise/Models/Cliente.cs
@@ -2,9 +2,11 @@
 {
     public class Cliente
     {
+        //Este archivo es una clase para la informacion a la hora de registrar un cliente.
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string DUI { get; set; }
         public string Telefono { get; set; }
     }
 }
+
